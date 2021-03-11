afc663d10d46936afb2955b4272f260c64f2c8aa
okti <62831711+okti-69@users.noreply.github.com>
2021-03-11 12:48:46 +0100

    Create crusty.py

---
+++ crusty.py
@@ -0,0 +1,41 @@
+# Takie info:
+# Nie jestem jakis py master wiec to jest glupio zrobione xD wazne ze dziala
+id_webhooka = "TU ID WEBHOOKA"
+token_webhooka = "TU TOKEN WEBHOOKA"
+
+
+
+import requests
+from discord import Webhook, AsyncWebhookAdapter, RequestsWebhookAdapter
+import aiohttp
+while True:
+    x = requests.post("http://crusty.pl")
+    stan_sharda_1 = x.text[13160:13270]
+    stan_sharda_2 = x.text[13520:13610]
+    print("SHARD 1:", stan_sharda_1.replace("<", "   ").replace(">", "   ").replace("br", "   "))
+    print("SHARD 2:", stan_sharda_2.replace("<", "   ").replace(">", "   ").replace("br", "   "))
+    print("_____________________________________________________________________________________")
+    if stan_sharda_1.find("niestabilny") == -1:
+        continue
+    else:
+        webhook = Webhook.partial(id_webhooka, token_webhooka,
+        adapter=RequestsWebhookAdapter())
+        webhook.send(stan_sharda_1)
+    if stan_sharda_1.find("pali") == -1:
+        continue
+    else:
+        webhook = Webhook.partial(id_webhooka, token_webhooka,
+        adapter=RequestsWebhookAdapter())
+        webhook.send(stan_sharda_1)
+    if stan_sharda_2.find("niestabilny") == -1:
+        continue
+    else:
+        webhook = Webhook.partial(id_webhooka, token_webhooka,
+        adapter=RequestsWebhookAdapter())
+        webhook.send(stan_sharda_2)
+    if stan_sharda_1.find("pali") == -1:
+        continue
+    else:
+        webhook = Webhook.partial(id_webhooka, token_webhooka,
+        adapter=RequestsWebhookAdapter())
+        webhook.send(stan_sharda_2)
